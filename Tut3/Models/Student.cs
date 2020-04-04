bb39efbdfcdccd5f0c9e76715bc3919a44eca0f1
ASUS <ASUS@INTEL>
2020-04-04 04:27:52 +0200

    :)

---
+++ Tut3/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tut3.Models
+{
+    public class Student
+    {
+        
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string BirthDate { get; set; }
+
+        public Enrollment enrollment { get; set; }
+
+    }
+}
